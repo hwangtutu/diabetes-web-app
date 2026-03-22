06df823d134555ef46319a37d1be19ee06bfb52a
hwangtutu <163239019+hwangtutu@users.noreply.github.com>
2026-03-23 04:07:52 +0900

    Update flask-diabetes.py

--- flask-diabetes.py
+++ flask-diabetes.py
@@ -68,4 +68,4 @@ def predict():
     return render_template('prediction.html', form=form)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
